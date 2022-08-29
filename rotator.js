d7f6bf0a87ae7ef6b5f287ac4fff7c60b63f4524
hape <hapemartini@gmail.com>
2022-08-29 17:42:24 +0200

    - add code to css files - pull js into document.onload()

--- rotator.js
+++ rotator.js
@@ -1,18 +1,25 @@
+document.onload(() => {
+  const initialText = document
+    .getElementsByClassName('notion-callout__content')[0]
+    .getElementsByTagName('h1')[0]
+    .getElementsByTagName('strong')[0]
+    .innerHTML
+  const firstWord = initialText.split(' ')[0]
 
-const initialText = document.getElementsByClassName('notion-callout__content')[0].getElementsByTagName('h1')[0].getElementsByTagName('strong')[0].innerHTML;
-const firstWord = initialText.split(' ')[0];
+  let rotatorReference = document
+    .getElementsByClassName('notion-callout__content')[0]
+    .getElementsByTagName('h1')[0]
+    .getElementsByTagName('strong')[0]
 
-let rotatorReference = document.getElementsByClassName('notion-callout__content')[0].getElementsByTagName('h1')[0].getElementsByTagName('strong')[0]
+  const rotatorWord = document.createElement('span')
+  rotatorWord.innerHTML = 'placeholder'
 
-const rotatorWord = document.createElement('span')
-rotatorWord.innerHTML = 'placeholder'
+  rotatorReference.appendChild(rotatorWord)
 
-rotatorReference.appendChild(rotatorWord)
-
-const words = [
-  'test 1',
-  'test 2',
-  'test 3',
+  const words = [
+    'test 1',
+    'test 2',
+    'test 3',
     //'IT-Berater', 
     //'Softwarearchitekt', 
     //'Full-Stack-Entwickler', 
@@ -20,18 +27,20 @@ const words = [
     //'Experte für Single-Page-Applications'
   ];
 
-const timer = (timout) => new Promise(resolve => setTimeout(resolve, timout));
+  const timer = (timout) => new Promise(resolve => 
+    setTimeout(resolve, timout));
 
-const rotate = async () => {
-  while(true) {
-    for (word of words) {
-      rotatorWord.innerHTML = word
-      rotatorWord.classList.toggle('fade-text')
-      await timer(3000)
-      rotatorWord.classList.toggle('fade-text')
-      await timer(1)
+  const rotate = async () => {
+    while (true) {
+      for (word of words) {
+        rotatorWord.innerHTML = word
+        rotatorWord.classList.toggle('fade-text')
+        await timer(3000)
+        rotatorWord.classList.toggle('fade-text')
+        await timer(1)
+      }
     }
   }
-}
 
-rotate()
+  rotate()
+})
